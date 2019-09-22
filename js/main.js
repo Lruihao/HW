ad0503e463ba39493994ed94043d3c4f82bcfe06
李瑞豪 <1074627678@qq.com>
2019-09-22 11:58:54 +0800

    0922

--- js/main.js
+++ js/main.js
@@ -27,6 +27,7 @@ window.onload = function () {
  */
 function toggle() {
   var aside = document.querySelector("aside");
+  var section = document.querySelector("section");
   if (aside.className === "close-aside") {
     aside.className = "open-aside";
   } else {
@@ -68,10 +69,10 @@ function createTime() {
   var now = new Date();
   var run = new Date("08/01/2019 11:30:00");
   //總的秒數
-  var runTime = (now - run) / 1000;
-  days = Math.floor(runTime / 60 / 60 / 24);
-  hours = Math.floor(runTime / 60 / 60 - (24 * days));
-  minutes = Math.floor(runTime / 60 - (24 * 60 * days) - (60 * hours));
+  var runTime = (now - run) / 1000,
+  days = Math.floor(runTime / 60 / 60 / 24),
+  hours = Math.floor(runTime / 60 / 60 - (24 * days)),
+  minutes = Math.floor(runTime / 60 - (24 * 60 * days) - (60 * hours)),
   seconds = Math.floor(runTime - (24 * 60 * 60 * days) - (60 * 60 * hours) - (60 * minutes));
   //前置零
   if (String(hours).length === 1) {
